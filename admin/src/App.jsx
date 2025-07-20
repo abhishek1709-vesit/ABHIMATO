a68e28c39a1b30eeeb2ce57f26df8b477f4189c0
abhishek1709-vesit <abhishekgore1706@gmail.com>
2025-07-20 11:24:40 +0530

    Changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,7 @@ import { Sidebar } from "./components/Sidebar"
 import { Add } from "./pages/Add"
 import { List } from "./pages/List"
 import { Orders } from "./pages/Orders"
+import { Home } from "./pages/Home"
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <div className="flex">
         <Sidebar/>
         <Routes>
+          <Route path="/" element={<Home/>}></Route>
           <Route path="/add" element = {<Add/>}></Route>
           <Route path="/list" element = {<List/>}></Route>
           <Route path="/orders" element = {<Orders/>}></Route>
---
+++ admin/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen flex-col">
+      <p className="text-6xl">Welcome to the</p>
+      <p className="text-4xl text-[tomato] pb-1 border-b-1">Admin Panel</p>
+    </div>
+  );
+};
